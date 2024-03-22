09746e55e8636954628aa5f2c48bb0e52b9c8f30
Alejandro G. <imthebest_1997@hotmail.com>
2024-03-21 21:40:28 -0500

    Node + Ts

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+]
+
+const findHeroById = (id: number) => {
+    return heroes.find(hero => hero.id === id);
+}
+
+const hero = findHeroById(7);
+
+console.log( hero?.name ?? 'Hero not found !');
