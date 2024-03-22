dc1feda9334ef411b8a96f094f2c84ea13bb95db
Alejandro G. <imthebest_1997@hotmail.com>
2024-03-21 22:13:01 -0500

    Refactor hero search logic in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,5 @@
-const heroes = [
-    {
-        id: 1,
-        name: 'Batman',
-        owner: 'DC'
-    },
-    {
-        id: 2,
-        name: 'Spiderman',
-        owner: 'Marvel'
-    },
-    {
-        id: 3,
-        name: 'Superman',
-        owner: 'DC'
-    },
-    {
-        id: 4,
-        name: 'Flash',
-        owner: 'DC'
-    },
-    {
-        id: 5,
-        name: 'Wolverine',
-        owner: 'Marvel'
-    },
-]
+import { findHeroById } from "./services/hero.service";
 
-const findHeroById = (id: number) => {
-    return heroes.find(hero => hero.id === id);
-}
-
-const hero = findHeroById(7);
+const hero = findHeroById( 2 );
 
 console.log( hero?.name ?? 'Hero not found !');
